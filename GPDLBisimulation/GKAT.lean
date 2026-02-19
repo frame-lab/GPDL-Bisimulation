e7b7437206a590555fd78b6279a8c7bdb48b83f1
Rafael Accetta <rafaelaccetta@id.uff.br>
2026-02-19 19:15:54 -0300

    bisimulation

--- GPDLBisimulation/GKAT.lean
+++ GPDLBisimulation/GKAT.lean
@@ -12,15 +12,14 @@ section
 variable
   {σ : Type u}
   {t : Type v}
-  {T : Finset t}
-  {X : Type w}
+  {T : List t}
   [DecidableEq t]
   [DecidableEq σ]
 
-inductive BExp (T : Finset t) : Type v
+inductive BExp (T : List t) : Type v
   | zero : BExp T
   | one : BExp T
-  | prim : T → BExp T
+  | prim : { b // b ∈ T } → BExp T
   | and : BExp T → BExp T → BExp T
   | or : BExp T → BExp T → BExp T
   | not : BExp T → BExp T
@@ -32,7 +31,7 @@ instance : Zero (BExp T) where
 instance : One (BExp T) where
   one := .one
 
-inductive Exp (σ : Type u) (T : Finset t) : Type (max u v)
+inductive Exp (σ : Type u) (T : List t) : Type (max u v)
   | do : σ → Exp σ T
   | assert : BExp T → Exp σ T
   | seq : Exp σ T → Exp σ T → Exp σ T
@@ -40,9 +39,11 @@ inductive Exp (σ : Type u) (T : Finset t) : Type (max u v)
   | while : BExp T → Exp σ T → Exp σ T
 deriving Repr
 
-def At (T : Finset t) := T.powerset
+def At' (T : Finset t) := T.powerset
 
-def eval (v : Finset t) : BExp T → Bool
+def At (T : List t) := T.sublists
+
+def eval (v : List t) : BExp T → Bool
   | 0 => false
   | 1 => true
   | .prim b =>
@@ -52,16 +53,16 @@ def eval (v : Finset t) : BExp T → Bool
   | .not b =>  ! eval v b
 
 
-inductive GuardedString (σ : Type u) (T : Finset t)
-  | final (state : At T) : GuardedString σ T
-  | cons (state : At T) (action : σ) (next : GuardedString σ T) : GuardedString σ T
+inductive GuardedString (σ : Type u) (T : List t)
+  | final (state : { s // s ∈ At T }) : GuardedString σ T
+  | cons (state : { s // s ∈ At T }) (action : σ) (next : GuardedString σ T) : GuardedString σ T
 
 inductive two where
   | zero : two
   | one : two
 deriving DecidableEq, BEq
 
-def G (σ : Type u) (T : Finset t) (X : Type w) := Finset t → (two ⊕ σ × X)
+def G (σ : Type u) (T : List t) (X : Type w) := List t → (two ⊕ σ × X)
 
 instance : Zero (two ⊕ σ × X) where
   zero := Sum.inl two.zero
@@ -69,11 +70,11 @@ instance : Zero (two ⊕ σ × X) where
 instance : One (two ⊕ σ × X) where
   one := Sum.inl two.one
 
-structure GCoalgebra (σ : Type u) (T : Finset t) where
+structure GCoalgebra (σ : Type u) (T : List t) where
   states : Nat
   map : Fin states → G σ T (Fin states)
 
-structure GAutomaton (σ : Type u) (T : Finset t) where
+structure GAutomaton (σ : Type u) (T : List t) where
   states : Nat
   map : Fin states → G σ T (Fin states)
   start : Fin states
@@ -209,57 +210,102 @@ def exp2automaton (e : Exp σ T) : GAutomaton σ T :=
     ⟨0, Nat.zero_lt_succ s⟩
   ⟩
 
-def accepting {X : GCoalgebra σ T} (s : Fin X.states) : Prop :=
-  ∃ α, X.map s α = 1
+def accepting {X : GCoalgebra σ T} (s : Fin X.states) : Bool :=
+  List.any (At T) (fun α ↦ X.map s α = 1)
 
-partial def live {X : GCoalgebra σ T} (s : Fin X.states) : Prop :=
-  ∃ α a b, X.map s α = .inr (a, b) ∧ (accepting b ∨ live b)
-
-def dead {X : GCoalgebra σ T} (s : Fin X.states) : Prop := ¬ live s
+def not_dead_states (X : GCoalgebra σ T) : Vector Bool X.states :=
+  let rec accepting_in_steps (N : Nat) : Vector Bool X.states :=
+    match N with
+    | 0 => Vector.map accepting (Vector.finRange X.states)
+    | n + 1 =>
+      (Vector.finRange X.states).map (fun x ↦
+        (accepting_in_steps n).get x ||
+        List.any (At T) (fun α ↦
+          match (X.map x α) with
+          | .inr (_, b) => (accepting_in_steps n).get b
+          | _ => false ))
+  accepting_in_steps X.states
 
 def normalize (X : GCoalgebra σ T) : GCoalgebra σ T :=
+  let not_dead_states_x := not_dead_states X
   ⟨
     X.states,
     fun x α ↦
       match X.map x α with
-      | .inr (a, b) =>
-          --if dead b then 0 else
-          X.map x α
+      | .inr (_, b) =>
+          if (not_dead_states_x.get b) then X.map x α else 0
       | _ => X.map x α
   ⟩
 
-def bisimulation (X Y : GAutomaton σ T) : Bool :=
+def inner_loop_aux (X Y : GAutomaton σ T) (x : Fin X.states) (y : Fin Y.states)
+      (A : List (List t)) (Q : Queue (Fin X.states × Fin Y.states)) :=
+      match A with
+      | [] => (Q, true)
+      | α::A' =>
+        match X.map x α, Y.map y α with
+        | .inl b1, .inl b2 =>
+          if b1 = b2
+          then inner_loop_aux X Y x y A' Q
+          else (Q, false)
+        | .inr (p1, x'), .inr (p2, y') =>
+          if p1 = p2
+          then inner_loop_aux X Y x y A' (Q.enqueue (x', y'))
+          else (Queue.empty, false)
+        | _, _ => (Queue.empty, false)
 
-  let inner_loop (x : Fin X.states) (y : Fin Y.states) (Q : Queue (Fin X.states × Fin Y.states))
-        (B : Bool) : Queue (Fin X.states × Fin Y.states) × Bool :=
-    let rec inner_loop_aux
-      (A : List (Finset t)) (Q' : Queue (Fin X.states × Fin Y.states)) (B : Bool) :=
-      if B then
-        match A with
-        | [] => (Q', B)
-        | α::A' =>
-          match X.map x α, Y.map y α with
-          | 0, 0 => (Q', B)
-          | 1, 1 => (Q', B)
-          | .inr (a1, b1), .inr (a2, b2) =>
-            if a1 = a2 then
-              let Q'' := Q.enqueue (b1, b2)
-              inner_loop_aux A' Q'' B
-            else (Queue.empty, false)
-          | _, _ => (Queue.empty, false)
-      else (Queue.empty, false)
-    inner_loop_aux (At T).toList Queue.empty B
-  /-let rec outer_loop (Q : Queue (Fin X.states × Fin Y.states)) (UF : UnionFind) (B : Bool) :=
+def inner_loop (X Y : GAutomaton σ T)
+                (x : Fin X.states) (y : Fin Y.states)
+                (Q : Queue (Fin X.states × Fin Y.states))
+                : Queue (Fin X.states × Fin Y.states) × Bool :=
+    inner_loop_aux X Y x y (At T) Q
+
+partial def outer_loop (X Y : GAutomaton σ T)
+                (Q : Queue (Fin X.states × Fin Y.states))
+                (UF : UnionFind) : Bool :=
     match Q.dequeue? with
-    | none => B
+    | none => true
     | some ⟨⟨x, y⟩, Q'⟩ =>
-      let ⟨UF', eq⟩ := UF.checkEquiv x y
+      let ⟨UF', eq⟩ := UF.checkEquiv! x (y + X.states)
       if eq then
-        outer_loop Q' UF' B
+        outer_loop X Y Q' UF'
       else
-        let ⟨Q'', B'⟩ := inner_loop x y Q' B
-        if B' then
-          let UF'' := UF'.union x y
-          outer_loop Q'' UF'' B'
-        else B' -/
-  sorry
+        let ⟨Q'', B'⟩ := inner_loop X Y x y Q'
+        if B'
+        then outer_loop X Y Q'' (UF'.union! x (y + X.states))
+        else false
+
+def bisimulation (X Y : GAutomaton σ T) : Bool :=
+  outer_loop X Y
+    (Queue.enqueue (X.start, Y.start) Queue.empty)
+    (UnionFind.mkEmpty (X.states + Y.states))
+
+def check_equivalence (e1 e2 : Exp σ T) : Bool :=
+  bisimulation (exp2automaton e1) (exp2automaton e2)
+
+#eval check_equivalence
+  (.do 'e' : Exp Char ['b'])
+  (.seq  (.assert 1) (.do 'e' ): Exp Char ['b'])
+
+#eval check_equivalence
+  (.if (.prim ⟨'b', List.mem_singleton_self 'b'⟩)
+    (.do 'e') (.do 'f'))
+  (.if (.not (.prim ⟨'b', List.mem_singleton_self 'b'⟩))
+    (.do 'f') (.do 'e'))
+
+#eval check_equivalence
+  (.while (.prim ⟨'b', List.mem_singleton_self 'b'⟩)
+    (.do 'e'))
+  (.while (.not (.prim ⟨'b', List.mem_singleton_self 'b'⟩))
+    (.do 'e'))
+
+/-
+#eval check_equivalence
+    (.while (.prim ⟨'b', List.mem_singleton_self 'b'⟩)
+      (.do 'e')  : Exp Char ['b'])
+    (.if (.prim ⟨'b', List.mem_singleton_self 'b'⟩)
+      (.seq
+        (.do 'e')
+        (.while (.prim ⟨'b', List.mem_singleton_self 'b'⟩)
+          (.do 'e')))
+      (.assert 1)  : Exp Char ['b'])
+-/
